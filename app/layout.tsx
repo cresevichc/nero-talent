3dd9c9d05e7ab838b47047d95de37a9e8836c8a1
Cristian <57160850+cresevichc@users.noreply.github.com>
2026-03-09 17:42:41 +0000

    fix imports and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import './globals.css';
 import { metadata } from "./metadata";
+import { Playfair_Display, Inter } from 'next/font/google';
+import { LanguageProvider } from '../context/LanguageContext';
 
 const playfair = Playfair_Display({
   subsets: ['latin'],
