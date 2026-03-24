c2cf22cb7c57766d55fae4d8deb14c9dde78fb07
Cristian <57160850+cresevichc@users.noreply.github.com>
2026-03-24 18:46:11 +0100

    Remove TEST div from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`bg-[#0D0D0F] text-[#F5F7FA] ${inter.className}`}>
         <div className="background-glow" />
-        <div>TEST</div>
+        
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
